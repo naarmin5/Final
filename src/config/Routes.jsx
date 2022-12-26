abe30bc1ff4afeb946b443cda7ef1fb05330ac9a
naarmin5 <115252716+naarmin5@users.noreply.github.com>
2022-12-27 02:02:39 +0400

    Add files via upload

---
+++ src/config/Routes.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Route, Switch } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Catalog from '../pages/Catalog';
+import Detail from '../pages/detail/Detail';
+import Contact from '../components/Contact/Contact';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route
+                path='/:category/search/:keyword'
+                component={Catalog}
+            />
+            <Route
+                path='/:category/:id'
+                component={Detail}
+            />
+            <Route
+                path='/:category'
+                component={Catalog}
+            />
+            <Route
+                path='/'
+                exact
+                component={Home}
+            />
+             <Route
+                path='/contact'
+                exact
+                component={Contact}
+            />
+
+        </Switch>
+    );
+}
+
+export default Routes;
